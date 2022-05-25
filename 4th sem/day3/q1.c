986d34dd2d1a764b3574738f1b1ace11afa1e097
Kibutsuji <tadisai091@gmail.com>
2022-05-25 20:10:48 +0530

    updated rotation logic

--- 4th sem/day3/q1.c
+++ 4th sem/day3/q1.c
@@ -4,22 +4,40 @@
 #include<stdlib.h>
 
 #define size 20
+int transpose[size][size];
 
-void rotate(int mat[][20],int *m,int *n) // m x n matrix
+void rotate(int mat[][20],int m,int n) // m x n matrix
 {
-    int i,j,temp;
-    for(j=0;j<*m;j++)
+    int i,j,k=0,temp;
+    //transpose of the matrix
+    for(i=0;i<m;i++)
     {
-        for(i=0;i<*n;i++)
+        for(j=0;j<n;j++)
         {
-            temp = mat[j][i];
-            mat[j][i] = mat[i][j];
-            mat[i][j] = temp;
+            transpose[j][i] = mat[i][j];
         }
     }
-    temp = *m;
-    *m = *n;
-    *n = temp;
+    // rotation logic
+    for(i=0;i<n/2;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            temp = transpose[i][j];
+            transpose[i][j] = transpose[n-i-1][j];
+            transpose[n-i-1][j] = temp;
+        }
+    }
+
+    //print the transpose of matrix
+    printf("Image matrix : \n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            printf("%d ",transpose[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void main()
@@ -35,14 +53,5 @@ void main()
             scanf("%d",&mat[i][j]);
         }
     }
-    rotate(mat,&m,&n);
-    printf("Image matrix : \n");
-    for(int i =0;i<m;i++)
-    {
-        for(int j =0;j<n;j++)
-        {
-            printf("%d ",mat[i][j]);
-        }
-        printf("\n");
-    }
+    rotate(mat,m,n);
 }
